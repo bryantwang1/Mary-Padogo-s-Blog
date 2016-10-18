e444ee613da4858fce71a7bd6151172d6c8f5fc5
Bryant Wang <bryant.m.wang@gmail.com>
2016-10-18 12:36:37 -0700

    Add buttons for light theme and dark theme, set default to light theme.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(function() {
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-theme");
+  });
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-theme");
+  });
+});
